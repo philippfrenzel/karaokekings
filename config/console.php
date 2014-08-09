1cfa4298e926c6dac083262e8a929efd77b6f5cd
Philipp Frenzel <philipp@frenzel.net>
2014-08-09 13:30:29 +0200

    update config

--- config/console.php
+++ config/console.php
@@ -15,6 +15,9 @@ return [
     ],
     'modules' => [
         'user' => 'dektrium\user\Module',
+        'pages'=>[
+            'class'=>'frenzelgmbh\scms\Module',
+        ]
     ],
     'components' => [
         'cache' => [
